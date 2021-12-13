aa07426e9d0fb4a892f3e057d7777548d15fd8e8
Qijia "Joy" Liu <joyliu.q@gmail.com>
2021-12-13 12:37:58 -0500

    :art: Add comments

--- utils/aggregate-sarif.js
+++ utils/aggregate-sarif.js
@@ -1,10 +1,11 @@
 // Reference: https://github.com/microsoft/sarif-tutorials/blob/main/docs/2-Basics.md
+// Reference: https://github.com/jduimovich/collect-sarif-files
 
 const fs = require('fs')
 const args = process.argv.splice(2)
 
 if (args.length < 3) {
-    console.log("You must pass two file names and a result file path to merge two files")
+    console.log("You must pass two file names and a result file path")
     console.log("Usage:", process.argv[0], " pathToSarif1 pathToSarif2 resultFilePath")
     process.exit(0)
 }
@@ -12,6 +13,7 @@ const mergeInto = args[0]
 const mergeFrom = args[1]
 const resultFilePath = args[2]
 
+// Actual execution
 fs.readFile(mergeInto, 'utf8', function (_, mergeIntodata) {
     fs.readFile(mergeFrom, 'utf8', function (_, mergeFromdata) {
         const merged = aggregate(mergeIntodata, mergeFromdata)
@@ -19,6 +21,31 @@ fs.readFile(mergeInto, 'utf8', function (_, mergeIntodata) {
     })
 })
 
+// Main aggregation logic
+function aggregate(s1, s2) {
+    let j1 = JSON.parse(s1)
+    let j2 = JSON.parse(s2)
+    
+    // If two sarifs originate from same tool (e.g. both semgrep), merge
+    if (fromSameTool(j1, j2)) {
+        return mergeRulesAndResults(j1, j2)
+    }
+    // Else, add as separate runs
+    else {
+        // TODO: even if they're from different tools, we probably want to
+        // get rid of repetition in some way. The solution is to either
+        // find some format that is able to label it as BOTH semgrep AND codeQL
+        // or prefer one tool's analysis & discard the other
+        const j1RunLength = j1.runs.length
+        const j2RunLength = j2.runs.length
+        for (let i = 0; i < j2RunLength; i++) {
+            j1.runs[j1RunLength + i] = j2.runs[i]
+        }
+        return j1
+    }
+}
+
+// Returns a boolean based on whether two sarif jsons originate from the same tool (e.g. "semgrep")
 function fromSameTool(j1, j2) {
     const driver1 = j1.runs[0].tool.driver.name
     const driver2 = j2.runs[0].tool.driver.name
@@ -33,25 +60,7 @@ function fromSameTool(j1, j2) {
     return driver1 == driver2
 }
 
-function aggregate(s1, s2) {
-    let j1 = JSON.parse(s1)
-    let j2 = JSON.parse(s2)
-    
-    // If two sarifs originate from same tool (e.g. both semgrep), merge
-    if (fromSameTool(j1, j2)) {
-        return mergeRulesAndResults(j1, j2)
-    }
-    // Else, add as separate runs
-    else {
-        const j1RunLength = j1.runs.length
-        const j2RunLength = j2.runs.length
-        for (let i = 0; i < j2RunLength; i++) {
-            j1.runs[j1RunLength + i] = j2.runs[i]
-        }
-        return j1
-    }
-}
-
+// Is only ran when the same tool is used: combines result & gets rid of repetition
 function mergeRulesAndResults(j1, j2) {
     let s1Rules = extractRules(j1)
     let s1Results = extractResults(j1)
@@ -145,9 +154,10 @@ function extractResults(sarif) {
     return results
 }
 
+// Simple function to write sarif into a file in JSON format
 function writeJSON(file, value, then) {
     let stream = fs.createWriteStream(file)
-    stream.once('open', function (fd) {
+    stream.once('open', function (_) {
         stream.write(JSON.stringify(value))
         stream.end()
         console.log("Created: ", file)
